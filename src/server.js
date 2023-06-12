9dbdc8965a47782ed396605fc04d8ac12f96400c
Anthoni Duong <aduong@LABA.PSWAS.LOCAL>
2023-06-12 13:44:01 -0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "/build")));
+
+app.listen(4000, () => {
+    console.log('listening on port', 4000)
+});
